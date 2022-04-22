90fad8716b53e69fb9713c10f5f2e8ffc5954656
allisongoldstein <81585138+allisongoldstein@users.noreply.github.com>
2022-04-22 18:32:49 -0500

    add login page

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,8 +1,14 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
