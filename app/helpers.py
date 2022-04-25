dfc527d32b53c5dd543f40ffd51dda9778884573
allisongoldstein <81585138+allisongoldstein@users.noreply.github.com>
2022-04-25 16:27:52 -0500

    format today in calendar

--- app/helpers.py
+++ app/helpers.py
@@ -5,11 +5,12 @@ def getMonthCalendar():
     today = date.today()
     month, year = today.month, today.year
 
-    cal = calendar.month(today.year, today.month)
+    # cal = calendar.month(today.year, today.month)
     monthCal = calendar.HTMLCalendar(firstweekday=0)
     calHTML = monthCal.formatmonth(year, month)
 
-    display = '<b><span class="today">' + str(today.day) + '</span></b>'
-    editedCal = calHTML.replace(str(today.day), display)
+    editDate = '>' + str(today.day) + '<'
+    display = '><b><span class="today">' + str(today.day) + '</span></b><'
+    editedCal = calHTML.replace(str(editDate), display)
     
     return editedCal
